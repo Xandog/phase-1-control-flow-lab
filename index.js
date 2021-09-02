66ba931ab290c0675da630b7ce9572cc5be17c09
Alex Aguirre <aguirre_alex@rocketmail.com>
2021-09-01 23:32:56 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if(someValue > 2500){
+    return 'No can do.';
+  }
+  else if(someValue > 2000){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if(someValue <= 400){
+    return 'This one is on me!';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someValue){
   // Write your code here!
+  return someValue === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someValue){
   // Write your code here!
+  let response;
+
+  switch(someValue){
+    case 'generous':
+      response = 'Thank you so much.'
+      break;
+    case 'not as generous':
+      response = 'Thank you.'
+      break;
+    default :
+      response = 'Bye.'
+      break;
+  }
+
+  return response;
 }
